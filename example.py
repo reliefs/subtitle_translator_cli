49fc91b3361d46850283af47d2efcc87de9c48e9
Steffan Jensen <6hourapp@gmail.com>
2019-03-30 21:43:34 +0100

    Update example.py

--- example.py
+++ example.py
@@ -1,5 +1,8 @@
 from subprocess import call
+import time
 
+print("Visit my github: http://github.com/reliefs/subtitle_translator_cli")
 wc = call("wc subtitle/w.srt | awk '{print $1}'", shell=True)
-print("There are so many lines", wc)
+print("^_^____ There are so many lines", wc)
+time.sleep(5)
 call('python run.py /folder/to/your/subtitle_translator/subtitle ' + str(wc) + ' yandex en da', shell=True)
